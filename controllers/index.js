44423a6c509af4b051157fdcb491122bb8917887
Pinzon Games <topuatumut@gmail.com>
2021-05-31 23:47:16 +0300

    MVC system update. Added controller

---
+++ controllers/index.js
@@ -0,0 +1,80 @@
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+function registerGET(req, res) {
+    let response = {
+        title: "Register",
+        error: req.query.error
+    }
+    res.render('register', response)
+}
+
+async function registerPOST(req, res) {
+    const exists = await User.exists({ username: req.body.username })
+    
+    if(exists) {
+        res.redirect('/register?error=true')
+        return
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err) return next(err)
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if(err) return next(err)
+            const newAdmin = new User({
+                username: req.body.username,
+                password: hash
+            })
+
+            newAdmin.save()
+
+            res.redirect('/login')
+        })
+    })
+}
+
+function loginGET(req, res) {
+    let response = {
+        title: "Login",
+        error: req.query.error
+    }
+    res.render('login', response)
+}
+
+function loginPOST() {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login?error=true'
+    })
+}
+
+function logout(req, res) {
+    req.logout()
+    res.redirect('/')
+}
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) return next()
+    res.redirect('/login')
+}
+
+function isLoggedOut(req, res, next) {
+    if(!req.isAuthenticated()) return next()
+    res.redirect('/')
+}
+
+function indexGET(req, res) {
+    res.render('index' , { title: "Home" })
+}
+
+module.exports = {
+    registerPOST,
+    registerGET,
+    isLoggedIn,
+    isLoggedOut,
+    loginGET,
+    loginPOST,
+    logout,
+    indexGET
+}
--- routes/index.js
+++ routes/index.js
@@ -1,74 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
-const bcrypt = require('bcrypt')
-const User = require('../models/user')
+const Controller = require('../controllers/index')
 
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) return next()
-    res.redirect('/login')
-}
-
-function isLoggedOut(req, res, next) {
-    if(!req.isAuthenticated()) return next()
-    res.redirect('/')
-}
-
-router.get('/', isLoggedIn, (req, res) => {
-    res.render('index' , { title: "Home" })
-})
-
-router.get('/login', isLoggedOut, (req, res) => {
-    let response = {
-        title: "Login",
-        error: req.query.error
-    }
-    res.render('login', response)
-})
-
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login?error=true'
-}))
-
-router.get('/register', isLoggedOut, (req, res) => {
-    let response = {
-        title: "Register",
-        error: req.query.error
-    }
-    res.render('register', response)
-})
-
+router.get('/', Controller.isLoggedIn, Controller.indexGET)
 router.post('/')
 
-router.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/')
-})
+router.get('/login', Controller.isLoggedOut, Controller.loginGET)
+router.post('/login', Controller.loginPOST)
 
-// Setup admin user. REMOVE THAT BEFORE PUBLISH
-router.post('/register', async (req, res) => {
-    const exists = await User.exists({ username: req.body.username })
-    
-    if(exists) {
-        res.redirect('/register?error=true')
-        return
-    }
+router.get('/register', Controller.isLoggedOut, Controller.registerGET)
+router.post('/register', Controller.registerPOST)
 
-    bcrypt.genSalt(10, (err, salt) => {
-        if(err) return next(err)
-        bcrypt.hash(req.body.password, salt, (err, hash) => {
-            if(err) return next(err)
-            const newAdmin = new User({
-                username: req.body.username,
-                password: hash
-            })
-
-            newAdmin.save()
-
-            res.redirect('/login')
-        })
-    })
-})
+router.get('/logout', Controller.logout)
 
 module.exports = router
